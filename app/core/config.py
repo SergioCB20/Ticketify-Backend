37b68875c2c38e6ff909618533df0b1afd3387a4
sash-gore <sasha.guzman@pucp.edu.pe>
2025-11-24 07:10:18 -0500

    se integró correo de confirmación

--- app/core/config.py
+++ app/core/config.py
@@ -20,8 +20,8 @@ class Settings(BaseSettings):
     API_VERSION: str = "v1"
 
     # URLs
-    FRONTEND_URL: str = "https://ones-duration-reserves-websites.trycloudflare.com"
-    BACKEND_URL: str = "https://unsinuated-shockheaded-chelsie.ngrok-free.dev"
+    FRONTEND_URL: str = "https://nottingham-portland-passion-newcastle.trycloudflare.com"
+    BACKEND_URL: str = "https://garishly-peckish-eula.ngrok-free.dev"
 
     # Database
     DATABASE_URL: str
